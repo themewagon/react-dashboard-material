dc16d41a64017d73cb29057a1498752d22b22c54
mostofanobi <mntanveer9196@gmail.com>
2024-05-07 15:19:06 +0600

    Updates

---
+++ precache-manifest.48b9a6ad29c8ce0251e42ce478b8e251.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841f7ceb201c62b921adb28396c42ad4",
+    "url": "/react-dashboard-material/index.html"
+  },
+  {
+    "revision": "374ced560004de5b3be1",
+    "url": "/react-dashboard-material/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "32f86679975a5f1bd6b6",
+    "url": "/react-dashboard-material/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "374ced560004de5b3be1",
+    "url": "/react-dashboard-material/static/js/2.bee81eb4.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/react-dashboard-material/static/js/2.bee81eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f86679975a5f1bd6b6",
+    "url": "/react-dashboard-material/static/js/main.3f92c6a7.chunk.js"
+  },
+  {
+    "revision": "feefcfa96284021451ec7e71a9f83892",
+    "url": "/react-dashboard-material/static/js/main.3f92c6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec52ea1870df3fa55da",
+    "url": "/react-dashboard-material/static/js/runtime-main.f6d5c7da.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/react-dashboard-material/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/react-dashboard-material/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/react-dashboard-material/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/react-dashboard-material/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/react-dashboard-material/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-dashboard-react/precache-manifest.7c05ec5787deb8143f5efd5b85d64a17.js"
+  "/react-dashboard-material/precache-manifest.48b9a6ad29c8ce0251e42ce478b8e251.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/material-dashboard-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-dashboard-material/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
